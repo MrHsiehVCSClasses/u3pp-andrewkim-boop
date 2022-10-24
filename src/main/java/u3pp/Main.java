237deda36ca067291a614e5607148181f31c9dbb
Andrew <andrew@Andrews-MacBook-Pro-2.local>
2022-10-24 10:00:35 -0700

    ily hsieh

--- src/main/java/u3pp/Main.java
+++ src/main/java/u3pp/Main.java
@@ -1,12 +1,16 @@
 package u3pp;
-
 public class Main 
 {
     public static void main(String[] args) {
-        
         // Use this space to test your code, or actually run your project
-        System.out.println("\n\nHi! This is an empty project.\nYour code will start running from Main.java.\nIt may be daunting to start a project from scratch, but try to have fun with it! :)\n");
-
+        // for some reason the maven tests did not work and so I had to make my own tests, but luckily the code does work
+        PalindromeTester myPalindrome = new PalindromeTester();
+        System.out.println(myPalindrome.isPalindrome("n"));
+        System.out.println(myPalindrome.isPalindrome("neveroddoreven"));
+        System.out.println(myPalindrome.isPalindrome("nEVERoddOREVen"));
+        System.out.println(myPalindrome.isPalindrome("()#!$*(%*("));
+        System.out.println(myPalindrome.isPalindrome("%%%$$%%%"));
+        System.out.println(myPalindrome.isPalindrome("narihsdiofasidfjaio"));
     }
 
 }
--- src/main/java/u3pp/PalindromeTester.java
+++ src/main/java/u3pp/PalindromeTester.java
@@ -1,5 +1,4 @@
 package u3pp;
-
 public class PalindromeTester {
 
     /**
@@ -7,8 +6,20 @@ public class PalindromeTester {
      * @param s  the string to test
      * @return true if the string is a palindrome
      */
-    public static boolean isPalindrome(String s) {
-
-        return true;
-    }
+    
+    public static boolean isPalindrome(String s)
+    {       
+        String reverse = "";
+        boolean test = false;
+        String lowerS = s.toLowerCase();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            String lower = Character.toString(c);
+            reverse += lower.toLowerCase();
+        }
+        if (lowerS.equals(reverse)) {
+            test = true;
+        }
+        return test;
+    } 
 }
